f8b20583af4d1cdc2d1077a6cbe591850a3c4a22
benjaminharper <daveshap37@gmail.com>
2018-03-12 14:02:57 -0400

    true rest svc
    true restful service that returns n seconds of recorded audio

--- app.py
+++ app.py
@@ -1,33 +1,48 @@
-import cv2
+import pyaudio
 from flask import Flask
 import json
 
-cam = cv2.VideoCapture(0)
+# global overhead stuff
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+CHUNK = 1024
+RECORD_SECONDS = 5
+WAVE_OUTPUT_FILENAME = "file.wav"
+audio = pyaudio.PyAudio()
+frames = []
+stream = audio.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK)
 
 
-def snapshot():
-    s, img = cam.read()
-    return json.dumps(img.tolist(), separators=(',', ':'))
+def grab_sound_block(data):
+    return frames 
 
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def default():
-    return str(snapshot())
+    return grab_sound_block(frames)
 
 
-@app.route("/left")
-def default():
-    return str(snapshot())
-
-
-@app.route("/right")
-def default():
-    return str(snapshot())
+def record_loop(loop_on):
+    while True:
+        if loop_on.value == True:
+            data = stream.read(CHUNK)
+            frames.append(data)
+        #time.sleep(1)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=6000)
-    #app.run()
+   recording_on = Value('b', True)
+   p = Process(target=record_loop, args=(recording_on,))
+   p.start()  
+   app.run(debug=True,
+           use_reloader=False,
+           host='0.0.0.0',
+           port=5000)
+   p.join()
