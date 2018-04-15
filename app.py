4ab49964955338cab70d1308114fabcdcf73f40a
David Shapiro <daveshap37@gmail.com>
2018-04-15 11:12:26 -0400

    started work on active listener

--- app.py
+++ app.py
@@ -1,36 +1,72 @@
+import math
+from collections import deque
+import audioop
 from flask import Flask
 import time
 import json
 import threading
 import pyaudio
-import sys
 
-frames = []
+frames = []  # container for audio samples
+channels = 1  # num audio channels
+rate = 44100  # samples per second
+chunk = 1024  # samples per chunk
+audio = pyaudio.PyAudio()  # audio device object
+depth = int(rate / chunk * 20)  # num seconds of audio to keep in memory
+silence = 2  # num seconds of silence to demarcate sounds i.e. all sounds within 2 seconds are part of same phrase
+threshold = 2500  # silence threshold
 
 
 def thread_recorder():
-    global frames
-    frames = []
-    format = pyaudio.paInt16
-    channels = 1
-    rate = 44100  # samples per second
-    chunk = 1024  # samples per chunk
-    audio = pyaudio.PyAudio()
-    depth = int(rate / chunk * 20)
-    # start recording
-    stream = audio.open(format=format,
+    stream = audio.open(format=pyaudio.paInt16,
                         channels=channels,
                         rate=rate,
                         input=True,
                         frames_per_buffer=chunk)
     while True:
         try:
-            data = stream.read(chunk)
-            frames.append(data.hex())
+            frame = stream.read(chunk)
+            frames.append(frame.hex())
             if len(frames) > depth:
                 frames.pop(0)
         except Exception as exc:
+            print(exc)
             pass
+
+
+def post_sound(clip):
+    print('posting clip to ???')
+
+
+def thread_listener():
+    stream = audio.open(format=pyaudio.paInt16,
+                        channels=channels,
+                        rate=rate,
+                        input=True,
+                        frames_per_buffer=chunk)
+    while True:
+        # wait for sound to start
+        print('listening for sound...')
+        audio2send = []
+        rel = rate / chunk
+        slid_win = deque(maxlen=silence * rel)
+        prev_audio = deque(maxlen=silence * rel)  # Prepend audio from 0.5 seconds before noise was detected
+        started = False
+        cur_data = stream.read(chunk)
+        slid_win.append(math.sqrt(abs(audioop.avg(cur_data, 4))))
+        if sum([x > threshold for x in slid_win]) > 0:
+            print('sound detected, recording...')
+            while True:
+                audio2send.append(cur_data)
+                started = False
+                slid_win = deque(maxlen=silence * rel)
+                prev_audio = deque(maxlen=0.5 * rel)
+                audio2send = []
+                tail_silence = 0
+                if tail_silence > silence:
+                    print('sound ended, sending now')
+                    post_sound(audio2send)
+                    break
 
 
 app = Flask(__name__)
@@ -40,14 +76,13 @@ app = Flask(__name__)
 def default():
     obj = {'payload': frames,
            'time': time.time()}
+    print('')
     return json.dumps(obj)
 
 
 if __name__ == "__main__":
-    thread = threading.Thread(target=thread_recorder)
-    thread.start()
-    if sys.argv[1] is int:
-        app.run(port = sys.argv[1])
-    else:
-        app.run(port=5000)
-        
+    recorder = threading.Thread(target=thread_recorder)
+    recorder.start()
+    listener = threading.Thread(target=thread_listener)
+    listener.start()
+    app.run(port=5000, debug=True)
