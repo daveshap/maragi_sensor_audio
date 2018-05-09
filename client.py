1870f1e9c87fe23ddd844187180072ccd8d3d4f1
David Shapiro <david.shapiro@tierpoint.com>
2018-05-09 16:14:17 -0400

    service and client updated and cleaned up a bit

--- client.py
+++ client.py
@@ -2,25 +2,31 @@ import flask
 import requests
 
 
-url = 'http://127.0.0.1:5001/client'
+app_uri = '/client'
+app_port = 5001
+app_url = 'http://127.0.0.1:%s%s' % (app_port, app_uri)
+mic_url = 'http://127.0.0.1:5000/mic'
 app = flask.Flask(__name__)
 
 
-@app.route("/client", methods=['GET', 'POST'])
+@app.route(app_uri)
 def default():
-    if flask.request.method == 'POST':
+    try:
         print(flask.request.form)
         return 'got it thanks!'
+    except Exception as exc:
+        print(exc)
+        return exc
 
 
 def subscribe():
     try:
-        response = requests.request('POST', 'http://127.0.0.1:5000/mic', data={'action': 'subscribe', 'url': url})
-        print(response)
+        response = requests.request('POST', mic_url, data={'action': 'subscribe', 'url': app_url})
+        print(response.text)
     except Exception as exc:
         print(exc)
 
 
 if __name__ == "__main__":
     subscribe()
-    app.run(port=5001)
+    app.run(port=app_port)
--- microservice.py
+++ microservice.py
@@ -1,58 +1,55 @@
 import requests
 import audioop
 import flask
-import time
-import json
 import threading
 import pyaudio
 
 #                                   flask stuff
 subscribers = []                    # list of urls to post sounds to
-port = 5000                         # this needs to be a system argument
-app = flask.Flask(__name__)
+app_port = 5000                     # this needs to be a system argument  TODO optional argument
+app_uri = '/mic'                    # api endpoint  TODO optional argument
+app = flask.Flask(__name__)         # flask app
+headers = {'Content-Type': 'application/octet-stream'}
 
 #                                   audio stuff
-frames = []                         # container for audio samples
 channels = 1                        # num audio channels
 rate = 16000                        # samples per second
 chunk = 1024                        # samples per chunk
 audio = pyaudio.PyAudio()           # audio device object
-depth = int(rate / chunk * 20)      # num of chunks to keep in memory
-silence = 2                         # num seconds of silence to demarcate sounds
-threshold = 2500                    # silence threshold
+silence = 1                         # num seconds of silence to demarcate sounds
+threshold = 2500                    # silence threshold  TODO auto adapt to ambient noise
+depth = int(rate / chunk * silence) # number of chunks to keep in memory
 
 
-def thread_recorder():
-    print('recorder started')
-    stream = audio.open(format=pyaudio.paInt16,
-                        channels=channels,
-                        rate=rate,
-                        input=True,
-                        frames_per_buffer=chunk)
-    while True:
-        try:
-            frame = stream.read(chunk)
-            frames.append(frame.hex())
-            if len(frames) > depth:
-                frames.pop(0)
-        except Exception as exc:
-            print(exc)
-            pass
-
-
-def post_sound(clip):
+def send_to_subscribers(frames):
+    data = b''.join(frames)
     for sub in subscribers:
         try:
-            response = requests.request('POST', sub, data={'time': time.time(), 'data': clip, 'rate': rate})
-            print('POST to', sub, response.status_code)
+            response = requests.request(method='POST', url=sub, data=data, headers=headers)
+            print('POST to', sub, response.text)
         except Exception as exc:
             print(exc)
 
 
-def thread_listener():
+def start_recording(stream, sound_clip):
+    tail_silence = 0
+    while True:
+        frame = stream.read(chunk)
+        sound_clip.append(frame)
+        rms = audioop.rms(frame, 2)
+        if rms < threshold:
+            tail_silence += 1
+        else:
+            tail_silence = 0
+        if tail_silence > depth:
+            print('sound ended, sending now')
+            send_to_subscribers(sound_clip)
+            return
+
+
+def listener():
     print('listener started')
     sound_clip = []
-    depth = int(rate / chunk * silence)
     stream = audio.open(format=pyaudio.paInt16,
                         channels=channels,
                         rate=rate,
@@ -66,61 +63,29 @@ def thread_listener():
             sound_clip.pop(0)
         if rms > threshold:
             print('sound detected, recording...')
-            audio2send = list()
-            audio2send.append(sound_clip)
-            tail_silence = 0
-            while True:
-                frame = stream.read(chunk)
-                audio2send.append(frame)
-                rms = audioop.rms(frame, 2)
-                if rms < threshold:
-                    tail_silence += 1
-                else:
-                    tail_silence = 0
-                if tail_silence > (rate / chunk * silence):
-                    print('sound ended, sending now')
-                    post_sound(audio2send)  # TODO send as binary raw audio file
-                    break
+            start_recording(stream, sound_clip)
 
 
-@app.route("/mic", methods=['GET', 'POST'])
+@app.route(app_uri)
 def default():
-    
-    if flask.request.method == 'GET':
-        # returns current 'frames', which is last 20 seconds of raw audio
-        # frames should be list of samples in the form of hex chunks
-        obj = {'data': frames, 'time': time.time(), 'rate': rate}
-        return json.dumps(obj)  # TODO send as binary raw audio file
-
-    elif flask.request.method == 'POST':
-        # expects object {action: (un)subscribe, url: http://blah:#/blah}
+    try:
         post = flask.request.form
         print(post)
-        try:
-            
-            if post['action'] == 'subscribe':
-                sub = post['url']
-                if sub in subscribers:
-                    return 'already subscribed!'
-                else:
-                    subscribers.append(sub)
-                    return 'subscribed'
-            
-            elif post['action'] == 'unsubscribe':
-                sub = post['url']
-                if sub in subscribers:
-                    subscribers.remove(sub)
-                    return 'unsubscribed'
-                else:
-                    return 'not a subscriber'
-        
-        except Exception as exc:
-            return str(exc)
+        sub = post['url']
+        if post['action'] == 'subscribe':
+            if sub in subscribers:
+                return 'subscribed'
+            subscribers.append(sub)
+            return 'subscribed'
+        elif post['action'] == 'unsubscribe':
+            subscribers.remove(sub)
+            return 'unsubscribed'
+    except Exception as exc:
+        print(exc)
+        return exc
 
 
 if __name__ == "__main__":
-    recorder = threading.Thread(target=thread_recorder)
-    recorder.start()
-    listener = threading.Thread(target=thread_listener)
-    listener.start()
-    app.run(port=port)
+    thread = threading.Thread(target=listener)
+    thread.start()
+    app.run(port=app_port)
