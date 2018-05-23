61df57abbc26f892565afeba00f5feacef7a86a4
David Shapiro <david.shapiro@tierpoint.com>
2018-05-23 12:35:21 -0400

    mic service has been simplified and streamlined. each sample is exactly 250ms and streams continuously

--- microservice.py
+++ microservice.py
@@ -1,82 +1,59 @@
 import time
 import uuid
 import requests
-import audioop
+import threading
 import pyaudio
 import json
 
 
-headers = {'Content-Type': 'application/octet-stream'}
 directory = 'http://127.0.0.1:5000/directory'
+phonebook = []
 channels = 1                        # num audio channels
-rate = 16000                        # samples per second
-chunk = 1024                        # samples per chunk
+rate = 16000                        # 16khz sample rate
+chunk = 4000                        # 250ms per audio chunk
 audio = pyaudio.PyAudio()           # audio device object
-silence = 1                         # num seconds of silence to demarcate sounds
-threshold = 2500                    # silence threshold TODO auto adapt to ambient noise
-depth = int(rate / chunk * silence) # number of chunks to keep in memory
 
 
-def publish_audio(frames, phonebook):
-    data = b''.join(frames)
+def publish_audio(data):
+    global phonebook
     t = time.time()
     u = uuid.uuid4()
     payload = {'time': str(t),
                'uuid': str(u),
                'type': 'raw_audio',
                'source': 'microphone service',
-               'data': str(data)}
+               'data': data}
     for service in phonebook:
         try:
             if service['input'] == 'raw_audio':
-                print('POST to', service)
+                #print('POST to', service)
                 response = requests.request(method='PUT', url=service['svc_url'], json=payload)
-                print(response)
+                #print(response)
         except Exception as exc:
             print(exc)
 
 
-def record_sound(stream, sound_clip):
-    tail_silence = 0
-    print('recording audio')
-    while True:
-        frame = stream.read(chunk)
-        sound_clip.append(frame)
-        rms = audioop.rms(frame, 2)
-        if rms < threshold:
-            tail_silence += 1
-        else:
-            tail_silence = 0
-        if tail_silence > depth:
-            print('recording ended')
-            return sound_clip
-
-
 def get_phonebook():
-    response = requests.request(method='GET', url=directory)
-    text = response.text
-    phonebook = json.loads(text)
-    return phonebook
+    # periodically updates the phonebook
+    global phonebook
+    while True:
+        response = requests.request(method='GET', url=directory)
+        text = response.text
+        phonebook = json.loads(text)
+        print('PHONEBOOK UPDATED', phonebook)
+        time.sleep(60)
 
 
 if __name__ == "__main__":
-    print('listener started')
-    sound_clip = []
+    phonebook_updater = threading.Thread(target=get_phonebook)
+    phonebook_updater.start()
     stream = audio.open(format=pyaudio.paInt16,
                         channels=channels,
                         rate=rate,
                         input=True,
                         frames_per_buffer=chunk)
+    print('audio stream running')
     while True:
         frame = stream.read(chunk)
-        sound_clip.append(frame)
-        rms = audioop.rms(frame, 2)
-        if len(sound_clip) > depth:
-            sound_clip.pop(0)
-        elif len(sound_clip) < depth:
-            continue
-        if rms > threshold:
-            audio_clip = record_sound(stream, sound_clip)
-            phonebook = get_phonebook()
-            publish_audio(audio_clip, phonebook)
-            sound_clip = []
+        publisher = threading.Thread(target=publish_audio(frame.hex()))
+        publisher.start()
