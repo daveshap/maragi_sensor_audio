446103520e04e6709816b65cbd9d2dfb17d2b121
David Shapiro <david.shapiro@tierpoint.com>
2018-07-02 13:58:40 -0400

    switching back to subscription based model

--- client.py
+++ client.py
@@ -3,24 +3,21 @@ import json
 import requests
 
 
-directory_url = 'http://127.0.0.1:5000/directory'
 app = flask.Flask(__name__)                                     # flask app
 app_port = 9999                                                 # port for this service to run on
 app_uri = '/test'                                               # uri endpoint
 app_ip = '127.0.0.1'                                            # ip address of local machine
 app_url = 'http://%s:%s%s' % (app_ip, app_port, app_uri)        # receiving url
+mic_url = 'http://127.0.0.1:6000/mic'
 
 
 @app.route(app_uri, methods=['PUT', 'GET'])
 def default():
     print(json.loads(flask.request.data))
-    return json.dumps({'result': 'got it!'})
+    return json.dumps({'result': 'audio client successfully received'})
 
 
 if __name__ == '__main__':
-    service = {'input': 'raw_audio',
-               'svc_url': app_url}
-    print('REGISTER', service)
-    response = requests.request(method='PUT', url=directory_url, json=service)
+    response = requests.request(method='POST', url=mic_url, json={'action': 'subscribe', 'url': app_url})
     print('RESPONSE', response)
     app.run(port=app_port)
--- microservice.py
+++ microservice.py
@@ -3,11 +3,14 @@ import uuid
 import requests
 import threading
 import pyaudio
+import flask
 import json
 
 
-directory = 'http://127.0.0.1:5000/directory'
-phonebook = []
+subscribers = []
+app_port = 6000
+app_uri = '/mic'
+app = flask.Flask(__name__)
 channels = 1                        # num audio channels
 rate = 16000                        # 16khz sample rate
 chunk = 4000                        # 250ms per audio chunk
@@ -22,35 +25,36 @@ def publish_audio(data):
                'sample_width': 16,
                'side': 'mono',
                'data': data.hex()}
-    for service in phonebook:
+    for url in subscribers:
         try:
-            if service['input'] == 'raw_audio':
-                resp = requests.request(method='PUT', url=service['svc_url'], json=payload)
-                print(resp.text)
+            resp = requests.request(method='PUT', url=url, json=payload)
+            print(resp.text)
         except Exception as exc:
             print('EXCEPTION', exc)
 
 
-def get_phonebook():
-    # periodically updates the phonebook
-    global phonebook
-    while True:
-        response = requests.request(method='GET', url=directory)
-        text = response.text
-        phonebook = json.loads(text)
-        print('PHONEBOOK UPDATED', phonebook)
-        time.sleep(60)
-
-
-if __name__ == "__main__":
-    phonebook_updater = threading.Thread(target=get_phonebook)
-    phonebook_updater.start()
-    stream = pyaudio.PyAudio().open(format=pyaudio.paInt16,
-                                    channels=channels,
-                                    rate=rate,
-                                    input=True)
+def audio_listener():
+    stream = pyaudio.PyAudio().open(format=pyaudio.paInt16, channels=channels, rate=rate, input=True)
     print('audio stream is open')
     while True:
         frame = stream.read(chunk)
-        publisher = threading.Thread(target=publish_audio(frame))
-        publisher.start()
+        threading.Thread(target=publish_audio(frame)).start()
+
+
+@app.route(app_uri, methods=['POST'])
+def default():
+    request = flask.request
+    payload = json.loads(request.data)
+    print(payload)
+    if request.method == 'POST':
+        if payload['action'] == 'subscribe':
+            if payload['url'] not in subscribers:
+                subscribers.append(payload['url'])
+                return json.dumps({'result': 'successfully added URL to subscribers list'})
+            else:
+                return json.dumps({'result': 'URL was already in subscribers list'})
+
+
+if __name__ == "__main__":
+    threading.Thread(target=audio_listener).start()
+    app.run(port=app_port)
