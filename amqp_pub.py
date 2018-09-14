02e0879bba08ced1f5215a0821e83cc84abcc45e
David Shapiro <daveshap37@gmail.com>
2018-09-14 18:55:13 -0400

    Update amqp_pub.py

--- amqp_pub.py
+++ amqp_pub.py
@@ -3,11 +3,13 @@ import pika
 
 
 def publish_audio():
+    print('opening audio stream')
+    stream = pyaudio.PyAudio().open(format=pyaudio.paInt16, channels=1, rate=16000, input=True)
+    print('opening AMQP connection')
     parameters = pika.URLParameters('amqp://guest:guest@maragi-rabbit:5672/%2F')
     connection = pika.BlockingConnection(parameters)
     channel = connection.channel()
-    stream = pyaudio.PyAudio().open(format=pyaudio.paInt16, channels=1, rate=16000, input=True)
-    print('audio stream is open')
+    print('starting publish loop')
     while True:
         frame = stream.read(4000)
         channel.basic_publish(exchange='sensor_audio', body=frame.hex(), routing_key='')
